c78839e8ad4cca48317ace936fdfcd2e505dc60f
Mikenike5313 <michaelkuhn@169-231-139-45.wireless.ucsb.edu>
2023-04-13 15:12:27 -0700

    mk - added Page.js file for San Diego

---
+++ frontend/src/main/pages/SanDiegoPage.js
@@ -0,0 +1,17 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function SanDiegoPage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>San Diego, CA</h1>
+        <p>
+            <b>San Diego, California</b> is a costal city near the southern border of California.  
+            It is known for being the hometown of the band Blink-182 as well as many micro-breweries.
+            Some popular places to go are the gaslamp district downtown, as well as La Jolla Shores near UCSD.
+        </p>
+        <p>Added by Michael K.</p>
+      </div>
+    </BasicLayout>
+  )
+}
