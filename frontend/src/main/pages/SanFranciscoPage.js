bea987ad77cb9f6e7f6276f16726d3ca545518c6
SophiaWeiler <sophia.weiler@outlook.com>
2023-04-12 17:49:46 -0700

    sw - add page for San Francisco

---
+++ frontend/src/main/pages/SanFranciscoPage.js
@@ -0,0 +1,16 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function SanFranciscoPage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>San Francisco, CA</h1>
+        <p>
+         <b>San Francisco, California</b> is a town and a county becuase the city is so big.
+         The main downtownis called "Union Square", and has shopping, food and museums nearby.
+        </p>
+        <p>Added by Sophia W.</p>
+      </div>
+    </BasicLayout>
+  )
+}
