9dda61177ef9cb8c1df49b434ca3fe2774c9fbf4
awal <ameymwalimbe@gmail.com>
2023-04-12 17:44:26 -0700

    aw - added route in App.js for New York

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,23 @@ import HomePage from "main/pages/HomePage";
 import AvilaBeachPage from "main/pages/AvilaBeachPage";
 import LosAlamosPage from "main/pages/LosAlamosPage";
 import ArroyoGrandePage from "main/pages/ArroyoGrandePage";
+import NewYorkPage from "main/pages/NewYorkPage";
 
 import "bootstrap/dist/css/bootstrap.css";
 
-
 function App() {
-
   return (
     <BrowserRouter basename="/team00-s23-5pm-4">
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/towns/AvilaBeach" element={<AvilaBeachPage />} />
         <Route exact path="/towns/LosAlamos" element={<LosAlamosPage />} />
-        <Route exact path="/towns/ArroyoGrande" element={<ArroyoGrandePage />} />
+        <Route
+          exact
+          path="/towns/ArroyoGrande"
+          element={<ArroyoGrandePage />}
+        />
+        <Route exact path="towns/NewYork" element={<NewYorkPage />} />
       </Routes>
     </BrowserRouter>
   );
